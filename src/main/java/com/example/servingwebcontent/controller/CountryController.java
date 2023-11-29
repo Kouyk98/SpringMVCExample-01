04c7acc9fe8186d4f0347a3aed0d3171b113cd83
mr.huang <mr.huang@localhost>
2023-11-30 00:25:37 +0900

    来自kou本地的更新

--- src/main/java/com/example/servingwebcontent/controller/CountryController.java
+++ src/main/java/com/example/servingwebcontent/controller/CountryController.java
@@ -4,18 +4,20 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.servingwebcontent.entity.CountryEntity;
+import com.example.servingwebcontent.form.CountryForm;
 import com.example.servingwebcontent.form.CountrySearchForm;
 import com.example.servingwebcontent.repository.CountryEntityMapper;
+import com.example.servingwebcontent.utils.CommonResult;
 import com.google.gson.Gson;
 
 @Controller
@@ -23,10 +25,18 @@ public class CountryController {
 
 	@Autowired
 	private CountryEntityMapper mapper;
+	
+	@Autowired
+	private CommonResult result;
+	
+	@Autowired
+	private Gson gson;
 
 	@GetMapping("/country")
-	public String init(CountrySearchForm countrySearchForm) {
+	public String init(Model model) {
 
+		model.addAttribute("searchForm", new CountrySearchForm());
+		model.addAttribute("countryForm", new CountryForm());
 		return "country/country";
 	}
 
@@ -57,13 +67,51 @@ public class CountryController {
 	 * 创建一个方法，监听/country/createCountry，
 	 * 实现根据请求的参数创建一个CountryEntity对象，并将其插入到数据库中。
 	 */
-	@PostMapping("/country/createCountry")
+	@PostMapping("/country/create")
 	@ResponseBody
-	public String createCountry(@RequestBody CountryEntity countryEntity) {
+	public String create(CountryForm countryForm) {
 		// Method body goes here
-		// For example, you might save the countryEntity to the database
+		CountryEntity countryEntity = new CountryEntity();
+		//将countryForm中的数据传给entity
+		countryEntity.setMstcountrycd(countryForm.getCd());
+		countryEntity.setMstcountrynanme(countryForm. getName());
+
+		
+		// you might save the countryEntity to the database
+		mapper.insert(countryEntity);
+		result.setStatus(0);
+		result.setMessage("数据加入成功");
 		// Then return a success message or the saved entity
-		return "Country created successfully";
+		return gson.toJson(result);
+	}
+	@PostMapping("/country/update")
+	@ResponseBody
+	public String update(CountryForm countryForm) {
+		// Method body goes here
+		CountryEntity countryEntity = new CountryEntity();
+		//将countryForm中的数据传给entity
+		countryEntity.setMstcountrycd(countryForm.getCd());
+		countryEntity.setMstcountrynanme(countryForm. getName());
+		
+		// you might save the countryEntity to the database
+		mapper.updateByPrimaryKey(countryEntity);
+		// Then return a success message or the saved entity
+		result.setStatus(0);
+		result.setMessage("数据更新成功");
+		return gson.toJson(result);
+	}
+	
+	@PostMapping("/country/delete")
+	@ResponseBody
+	public String delete(CountryForm countryForm) {
+		// Method body goes here
+		
+		// you might save the countryEntity to the database
+		mapper.deleteByPrimaryKey(countryForm.getCd());
+		// Then return a success message or the saved entity
+		result.setStatus(0);
+		result.setMessage("数据删除成功");
+		return gson.toJson(result);
 	}
 
 }
--- src/main/resources/static/js/country.js
+++ src/main/resources/static/js/country.js
@@ -1,53 +1,106 @@
-$(document).ready(function () {
-    $("#detailContains").css("display", "none");
-    // when click the create button, show the detailContains
-    $("#selCreate").on('click', function () {
-        // clear all input
-        $(':input', '#frmDetail')
-            .not(':button, :submit, :reset, :hidden')
-            .val(''); 
-        // show the detailContains
-        $("#detailContains").css("display", "block");
-        // hide the queryContainer
-        $("#queryContainer").css("display", "none");
-    });
+let operation = 0;//初始化
+$(document).ready(function() {
+	$("#detailContains").css("display", "none");
+	// when click the create button, show the detailContains
+	$("#selCreate").on('click', function() {
+		// clear all input
+		$(':input', '#frmDetail')
+			.not(':button, :submit, :reset, :hidden')
+			.val('');
+		// show the detailContains
+		$("#detailContains").css("display", "block");
+		// hide the queryContainer
+		$("#queryContainer").css("display", "none");
+		operation = 1;//新建标识
+	});
 
-    // when click the update button, show the queryContainer
-    $("#selUpdate, #selDelete").on('click', function () {
-        // show the queryContainer
-        $("#queryContainer").css("display", "block");
-        // hide the detailContains
-        $("#detailContains").css("display", "none");
-        // set the form action for update
-        $("#frmDetail").attr("action", "/UpdateCountry");
-    });
+	// when click the update button, show the queryContainer
+	$("#selUpdate, #selDelete").on('click', function() {
+		// show the queryContainer
+		$("#queryContainer").css("display", "block");
+		// hide the detailContains
+		$("#detailContains").css("display", "none");
+		// set the form action for update
+		$("#frmDetail").attr("action", "/UpdateCountry");
+		if ($(this).attr("id") == "selUpdate") {
+			operation = 2;//选择更新时标识为2
+		} else {
+			operation = 3;//选择删除时标识为3
+		}
+	});
 
-    // when click the return button, hide the detailContains
-    $("#returnBtn").on('click', function () {
-        // show the queryContainer
-        // $("#queryContainer").css("display", "block");
-        // hide the detailContains
-        $("#detailContains").css("display", "none");
-    });
+	// when click the return button, hide the detailContains
+	$("#returnBtn").on('click', function() {
+		// show the queryContainer
+		// $("#queryContainer").css("display", "block");
+		// hide the detailContains
+		$("#detailContains").css("display", "none");
+	});
 
-    $("#queryBtn").on('click', function () {
-        // use ajax to post data to controller
-        // recived the data from controller with json
-        // show the data in the detailContains
-        $.ajax({
-            type: "POST",
-            url: "/country/getCountry",        //  <- controller function name
-            data: $("#frmSearch").serialize(),
-            dataType: 'json',
-            success: function (data) {
-                $("#detailContains").css("display", "block");
-                // show the data in the detailContains
-                $("#countryCodeInput").val(data.mstcountrycd);
-                $("#countryNameInput").val(data.mstcountrynanme);
-            },
-            error: function (e) {
-                alert("error");
-            }
-        });
-    });
+	$("#queryBtn").on('click', function() {
+		// use ajax to post data to controller
+		// recived the data from controller with json
+		// show the data in the detailContains
+		$.ajax({
+			type: "POST",
+			url: "/country/getCountry",        //  <- controller function name
+			data: $("#frmSearch").serialize(),
+			dataType: 'json',
+			success: function(data) {
+				$("#detailContains").css("display", "block");
+				// show the data in the detailContains
+				//更新时固定国家代码
+				if (operation == 2){
+				$("#cd").val(data.mstcountrycd).prop('readonly', true);
+				$("#name").val(data.mstcountrynanme);
+				}
+				//删除时固定国家代码和国名
+				if (operation == 3){
+				$("#cd").val(data.mstcountrycd).prop('readonly', true);
+				$("#name").val(data.mstcountrynanme).prop('readonly', true);
+				}
+				
+
+			},
+			error: function(e) {
+				alert("error");
+			}
+		});
+	});
+	//updateBtn绑定事件，根据operation的值判断提交表单的URL
+	$("#updateBtn").on('click', function() {
+		var url = " ";
+		if (operation == 3) {
+			url = "/country/delete";
+		} else if (operation == 2) {
+			url = "/country/update";
+		} else if (operation == 1) {
+			url = "/country/create";
+		} else {
+			alert("operation error");
+			return;
+		}
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: $("#frmDetail").serialize(),
+			dataType: 'json',
+			success: function(data) {
+				if (data.status == 0) {
+					alert(data.message);
+					return;
+				} else {
+					alert("Data insert failed");
+					return;
+				}
+			}, complete: function() {
+				// 更新后，清空查询输入框，解除details框固定
+				$("#frmDetail :input").val('');
+				$("#cd").prop('readonly', false);
+				$("#name").prop('readonly', false);
+				
+			}
+		})
+	});
+
 });
